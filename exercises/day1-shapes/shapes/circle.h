9dae1ea8e85a51d86f36731e6e33e0a1ea0c63c6
Joerg Faschingbauer <jf@faschingbauer.co.at>
2023-03-21 13:43:06 +0100

    shapes: add circle, square

--- exercises/day1-shapes/shapes/circle.h
+++ exercises/day1-shapes/shapes/circle.h
@@ -2,21 +2,15 @@
 
 #include "point.h"
 
-#include <cmath>
-
-
 class Circle
 {
 public:
-    Circle(const Point& center, int radius)
-    : _center(center),
-      _radius(radius)
-    {}
+    Circle(const Point& center, int radius) : _center(center), _radius(radius) {}
 
     const Point& center() const { return _center; }
     int radius() const { return _radius; }
 
-    double area() const { return M_PI * _radius * _radius; }
+    double area() const;
 
 private:
     Point _center;
